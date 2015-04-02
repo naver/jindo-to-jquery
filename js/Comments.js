7dd11d728c0f91242ed51c8eca4c14b792c80b8b
jongmoon <jongmoon@Naverui-MacBook-Pro.local>
2015-04-02 17:25:26 +0900

    디스커스 적용

--- js/Comments.js
+++ js/Comments.js
@@ -2,16 +2,15 @@ var Comments = (function() {
 	return {
 		show : function($parent) {
 			//disquss 불필요한 데이터가 쌓이는 것을 방지하기 위해 임시로 DISABLE 시켜놓음 2015.03.31
-			//var disqusTpl = _.template($("#disqus-template").text());
-			//var disqusHtml = disqusTpl();
-			//$('#code_comments').html(disqusHtml);
+			var disqusTpl = _.template($("#disqus-template").text());
+			var disqusHtml = disqusTpl();
 
 			if ($('#code_comments').length == 0) {
 				$parent.append('<div class="comments" id="code_comments">/');
 			}
 
-			$('#code_comments').html('<img src="./img/comments.png">');
-
+			//$('#code_comments').html('<img src="./img/comments.png">');
+			$('#code_comments').html(disqusHtml);
 		},
 		hide : function() {
 			$('#code_comments').html('');
