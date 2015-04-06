eb3301e5f8d89ce6bdaf4bd404452903aa309e35
jongmoon <jongmoon@Naverui-MacBook-Pro.local>
2015-04-06 10:16:33 +0900

    가이드 문서 갱신

--- js/data/guideList.js
+++ js/data/guideList.js
@@ -1,11 +1,17 @@
 var GUIDES = [{
 	title : '마이그레이션 안내',
 	content : [
-		{title : '마이그레이션 사용법', url : 'html/guide.usage.html'},
-		{title : '10분 요리', content : [
+		{title : '소개', url : 'html/guide.usage.html'},
+		{title : 'jQuery 기초튼튼', content : [
+			{title : '작동원리 기본', url : 'html/basic/guide.basic.how-jquery-works.html'},
+			{title : '이벤트 기본', url : 'html/basic/guide.basic.event.html'},
+			{title : '이펙트 기본', url : 'html/basic/guide.basic.effect.html'},
+			{title : 'Ajax 기본', url : 'html/basic/guide.basic.ajax.html'},
+			{title : '플러그인 기본', url : 'html/basic/guide.basic.plugin.html'},
+			{title : '성능 기본', url : 'html/basic/guide.basic.performance.html'}
+		]},
+		{title : '1:1 매핑이 어려운 부분', content : [
 			{title : '선택하기', url : 'html/guide.10min.html'},
-			{title : '변경하기', url : 'html/guide.10min.html'},
-			{title : '붙이고 떼기', url : 'html/guide.10min.html'}
 		]}
 	]}
 ];
