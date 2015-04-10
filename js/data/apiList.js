1e139c0af8e09acd5e299b31ff226fcd65f0950a
jongmoon <jongmoon@Naverui-MacBook-Pro.local>
2015-04-10 11:37:30 +0900

    Jindo API 대소문자 오류 수정

--- js/data/apiList.js
+++ js/data/apiList.js
@@ -46,12 +46,12 @@ var JINDO_APIS = [{
 	"className": "$A",
 	"property": [],
 	"methods": [{
-		"name": "break()",
+		"name": "Break()",
 		"description": "Break() 메서드는 forEach(), filter(), map() 메서드의 루프를 중단한다. 내부적으로는 강제로 예외를 발생시키는 구조이므로, try - catch 영역에서 이 메서드를 실행하면 정상적으로 동작하지 않을 수 있다.",
 	 	"jindoCodeUrl": "nozuhi",
 		"jqueryCodeUrl": "higepi"
 	}, {
-		"name": "continue()",
+		"name": "Continue()",
 		"description": "Continue() 메서드는 forEach(), filter(), map() 메서드의 루프에서 나머지 명령을 실행하지 않고 다음 루프로 건너뛴다. 내부적으로는 강제로 예외를 발생시키는 구조이므로, try - catch 영역에서 이 메서드를 실행하면 정상적으로 동작하지 않을 수 있다.",
 		"jindoCodeUrl": "fusupa",
 		"jqueryCodeUrl": "befuvo"
